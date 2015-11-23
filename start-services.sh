beded905967d3851835d4717c5c5ac9f4098c12e
Chris Hubbard <chris_hubbard@sil.org>
2015-11-23 16:31:55 -0500

    Add whenurlavail to delay swarm client
    There is a timing issue with jenkins master and slave starting
    at the same time with docker.  Introduce a wait until Jenkins
    master is ready for slave to connect.

--- start-services.sh
+++ start-services.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
+# There is a timing issue in the swarm-client.  The startup
+# for the jenkins is now faster and the appbuilder-agent
+# tends to get stuck starting up.  By waiting until
+# JENKINS_URL/plugin/swarm/slaveInfo is available allows
+# for clean startup.
+# This is needed in production as well.  It is OK to have
+# this startup dependency with AWS ECS since Jenkins 
+# master and slave are one task definition.  If these are
+# split up into separate task definitions, then a correct
+# fix of swarm-client will need to be implemented.
+for i in `seq 1 $#`;
+do
+opt=${@:$i:1}
+case $opt in
+	-master)
+	URL=${@:$i+1:1}
+	;;
+esac
+done
+
+if [ -n "$URL" ]; then
+whenurlavail "${URL}/plugin/swarm/slaveInfo" 10 0 java -jar /opt/swarm-client-jar-with-dependencies.jar "$@"
+else
 java -jar /opt/swarm-client-jar-with-dependencies.jar "$@"
+fi
